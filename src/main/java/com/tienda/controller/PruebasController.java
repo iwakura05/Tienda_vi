8652baf152787b63d1167fac74d019d2abda44d0
Fiorella <fiorella120120@gmail.com>
2025-04-15 02:53:22 -0600

    Semana 9 consultas ampliadas, JPQL y SQL

--- src/main/java/com/tienda/controller/PruebasController.java
+++ src/main/java/com/tienda/controller/PruebasController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pruebas")
@@ -15,7 +17,7 @@ public class PruebasController {
 
     @Autowired
     private ProductoService productoService;
-    
+
     @Autowired
     private CategoriaService categoriaService;
 
@@ -23,18 +25,60 @@ public class PruebasController {
     public String listado(Model model) {
         var lista = productoService.getProductos(false);
         model.addAttribute("productos", lista);
-        var categorias=categoriaService.getCategorias(false);
-        model.addAttribute("categorias", categorias); 
-        return "/pruebas/listado";
-    }
-    
-    @GetMapping("/listado/{idCategoria}")
-    public String listado(Model model, Categoria categoria) {
-        categoria=categoriaService.getCategoria(categoria);
-        model.addAttribute("productos", categoria.getProductos());
-        var categorias=categoriaService.getCategorias(false);
-        model.addAttribute("categorias", categorias); 
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
         return "/pruebas/listado";
     }
 
+    @GetMapping("/listado/{idCategoria}")
+    public String listado(Model model, Categoria categoria) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("productos", categoria.getProductos());
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+    }
+
+    @GetMapping("/listado2")
+    public String listado2(Model model) {
+        var lista = productoService.getProductos(false);
+        model.addAttribute("productos", lista);
+        return "/pruebas/listado2";
+    }
+
+    @PostMapping("/query1")
+    public String query1(
+            @RequestParam("precioInf") double precioInf,
+            @RequestParam("precioSup") double precioSup,
+            Model model) {
+        var lista = productoService.consultaAmpliada(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+    
+      @PostMapping("/query2")
+    public String query2(
+            @RequestParam("precioInf") double precioInf,
+            @RequestParam("precioSup") double precioSup,
+            Model model) {
+        var lista = productoService.consultaJPQL(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+    
+      @PostMapping("/query3")
+    public String query3(
+            @RequestParam("precioInf") double precioInf,
+            @RequestParam("precioSup") double precioSup,
+            Model model) {
+        var lista = productoService.consultaSQL(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
 }
--- src/main/java/com/tienda/repository/ProductoRepository.java
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -1,8 +1,35 @@
 package com.tienda.repository;
 
 import com.tienda.domain.Producto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ProductoRepository extends JpaRepository<Producto,Long>{
-    
+public interface ProductoRepository extends 
+        JpaRepository<Producto, Long> { // SE NECESITA QUE SEA QUI PARA QUE FUNCIONE
+
+    //Consulta ampliada
+    public List<Producto> findByPrecioBetweenOrderByPrecio(
+            double precioInf, //BUSACR POR PRECIO
+            double precioSup);
+
+    //Consulta JPQL
+    @Query(value="SELECT a"
+            + " FROM Producto a"
+            + " WHERE a.precio"
+            + " BETWEEN :precioInf AND :precioSup"
+            + " ORDER BY a.precio") // A ES UN ALIAS
+    public List<Producto> consultaJPQL(
+            double precioInf,
+            double precioSup);
+   
+    //Consulta SQL
+    @Query(nativeQuery=true,value="SELECT * "
+            + "FROM producto a"
+            + " WHERE a.precio"
+            + " BETWEEN :precioInf AND :precioSup "
+            + "ORDER BY a.precio")
+    public List<Producto> consultaSQL(
+            double precioInf,
+            double precioSup);
 }
