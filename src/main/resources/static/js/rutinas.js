686a34c3c002b873448fb8788e70a6ddbe61ac35
Fiorella <fiorella120120@gmail.com>
2025-04-16 04:16:09 -0600

    Semana 12 carrito 1-2

--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,16 +1,28 @@
 //Esta funcion carga una imagen local en la pagina html
-function readURL(input){
-    if (input.files && input.files[0]){
+function readURL(input) {
+    if (input.files && input.files[0]) {
         //Nos pasaron un archivo de imagen
         var lector = new FileReader();
-        lector.onload = function(e){
-          $('#blah').attr('src',e.target.result)
-                  .height(200);
+        lector.onload = function (e) {
+            $('#blah').attr('src', e.target.result)
+                    .height(200);
         };
-        
+
         lector.readAsDataURL(input.files[0]);
-        
+
     }
 }
 
+//Esta funcion carga en el carrito de compras un producto seleccionado por el usuario
+function addCart(formulario) {
+    var idProducto = formulario.elements[0].value;
+    var existencias = formulario.elements[1].value;
+    if (existencias > 0) {
+        //se incluye en el carrito
+        var ruta = "/carrito/agregar/" + idProducto;
+        $("#resultBlock").load(ruta);
+    } else {
+        window.alert("No hay existencias...");
+    }
+}
 
